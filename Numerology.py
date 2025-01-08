fa7ddf1b88ea210dcb4b10d02966ff745f697a60
Sourabh <SOurab@gmail.com>
2025-01-08 22:21:58 +0530

    Final Python Software
    In Dist Folder

--- Numerology.py
+++ Numerology.py
@@ -23,32 +23,35 @@ def on_enter_press(event):
 def count_length():
     text = entry.get().upper()
     result = calculate_string_value(text)
-    result_label.config(text=f"String Value: {result}")
+    result_label.config(text=f" Value: {result}", font=("Arial", 25))  
 
 window = Tk()
 window.title("Alphabet Value Calculator")
 
-window_width = 400
-window_height = 200
+window_width = 500
+window_height = 500 
 screen_width = window.winfo_screenwidth()
 screen_height = window.winfo_screenheight()
 x_coordinate = (screen_width - window_width) // 2
 y_coordinate = (screen_height - window_height) // 2
 window.geometry(f"{window_width}x{window_height}+{x_coordinate}+{y_coordinate}")
 
-label = Label(window, text="Enter Name:", font=("Helvetica", 12))
+title_label = Label(window, text="Numerology Calculator", font=("Arial", 16, "bold"))
+title_label.pack(pady=20)
+
+label = Label(window, text="Enter Name:", font=("Arial", 14))
 label.pack(pady=15)
 
-entry = Entry(window, width=50, font=("Helvetica", 12))
+entry = Entry(window, width=50, font=("Arial", 12))
 entry.pack()
 entry.bind('<Return>', on_enter_press) 
 
-button = Button(window, text="Calculate", font=("Helvetica", 12), bg="#4CAF50", fg="white", padx=20, pady=10)
+button = Button(window, text="Calculate", font=("Arial", 12), bg="#4CAF50", fg="white", padx=20, pady=10)
 button.pack(pady=15)
 
-result_label = Label(window, text="", font=("Helvetica", 12))
+result_label = Label(window, text="", font=("Arial", 50))  
 result_label.pack(pady=15)
 
-window.configure(bg="#f0f0f0")  # Light grey background
+window.configure(bg="#f0f0f0") 
 
 window.mainloop()
